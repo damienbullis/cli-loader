82af82296fa677c6d985e06a2954350957bc7de5
Damien <damien@bodhi.digital>
2023-03-31 22:52:31 -0500

    better animation style

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,25 @@
 import chalk from "chalk";
 import ansi from "ansi-escapes";
 
-// Could do it something like this as well...
-// const b = [
-//   5,
-//   4,
-//   3,
-//   2,
-//   1,
-//   0,
-//   0,
-//   0,
-//   0,
-//   0,
-// ]
-
 const spinner = ["◜", "◠", "◝", "◞", "◡", "◟"]; // Spinner animation frames
 const loadingBarWidth = 10; // Width of the loading bar in characters
 const barGradient = [
   chalk.rgb(253, 247, 195),
-  chalk.rgb(255, 222, 180),
-  chalk.rgb(255, 180, 180),
-  chalk.rgb(244, 164, 255),
   chalk.rgb(244, 133, 255),
-] as const;
+  chalk.rgb(244, 164, 255),
+  // chalk.rgb(255, 180, 180),
+  chalk.rgb(255, 222, 180),
+];
 
 const _loadingBar = Array.from({ length: loadingBarWidth }, (_, i) => {
-  const diff = loadingBarWidth - barGradient.length;
-  if (i >= diff) {
-    return barGradient[i - diff]!;
-  } else {
-    return barGradient[0];
-  }
+  return barGradient[0]!;
 });
 
 const loadingBar = () => {
+  // Inject gradients into the loading bar
+  if (barGradient && barGradient.length > 0) {
+    _loadingBar[0] = barGradient.shift()!;
+  }
   const end = _loadingBar.pop()!;
   _loadingBar.unshift(end);
   const bar = _loadingBar.map((c) => c("█"));
